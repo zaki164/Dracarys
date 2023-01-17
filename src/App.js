731589c4435acda7c8a79650bcee1be57183c973
zaki <92805900+zaki164@users.noreply.github.com>
2023-01-17 18:34:00 +0200

    enhance html file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
+// import "bootstrap/dist/js/bootstrap.bundle.min";
 import 'aos/dist/aos.css';
 import './sass/App.scss';
 import { HashRouter as Router } from 'react-router-dom';
