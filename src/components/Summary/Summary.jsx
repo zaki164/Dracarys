6d6cb75c429c919eaaceca57d8fb95b04d1837f6
zaki <92805900+zaki164@users.noreply.github.com>
2023-02-14 10:25:06 +0200

    summary with user

--- src/components/Summary/Summary.jsx
+++ src/components/Summary/Summary.jsx
@@ -1,13 +1,15 @@
 import { useRef, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { useAuth0 } from '@auth0/auth0-react';
 import './Summary.scss';
 
 const Summary = () => {
+  const { user } = useAuth0();
   const summref = useRef();
   useEffect(() => {
     let myps = [...summref.current.children];
     let i = 0;
-    setInterval(() => {
+    const interval = setInterval(() => {
       myps.forEach(ele => {
         ele.classList.remove('active');
       })
@@ -17,13 +19,13 @@ const Summary = () => {
         i = 0;
       }
     }, 5000);
-    // return () => {
-    //   clearInterval(interval)
-    //   console.log('return')
-    // }
-  }, [])
+    return () => {
+      clearInterval(interval)
+    }
+  }, [user])
   return (
     <div className="summary" ref={summref}>
+      {user && <p>Welcome <span>{user.name}</span>, We have all you need </p> }
       <p>Summer Sale Discount Off 50%, <Link to="/">Shop Now</Link></p>
       <p className='active'>The Dracarys Themes.</p>
       <p>We are Beside you, <Link to="/">Search For Us</Link></p>
