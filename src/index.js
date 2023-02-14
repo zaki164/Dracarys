476c643710db32b509d0d1db2404ef94dbab001e
zaki <92805900+zaki164@users.noreply.github.com>
2023-02-14 11:53:16 +0200

    auth0 redirect_uri commit

--- src/index.js
+++ src/index.js
@@ -15,7 +15,7 @@ root.render(
       domain={domain}
       clientId={clientId}
       authorizationParams={{
-        redirect_uri: window.location.origin
+        redirect_uri: "https://zaki164.github.io/Dracarys/"
       }}
     >
       <App />
